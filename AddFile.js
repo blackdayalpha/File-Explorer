3d7f4259dc46a001d9f4a0437193dc0002b23b6a
blackdayalpha <93610550+blackdayalpha@users.noreply.github.com>
2024-02-12 09:54:10 +0530

    Update AddFile.js

--- AddFile.js
+++ AddFile.js
@@ -36,32 +36,71 @@ Ext.define('Explorer.view.main.AddFile', {
                 var fileName = this.up('form').getForm().getFieldValues().fileName;
                 let selectedNode = Ext.getCmp('expView').getSelection();
 
-                var title = this.lookupViewModel().get('title');
-                // console.log('Title:', title);
-                let isLeaf = true;
-                if (title === 'Folder') {
-                    // console.log('inside folder')
-                    isLeaf = false;
-                }
-                if (selectedNode && selectedNode.length == 1 && fileName) {
-                    let newNode = Ext.create('Ext.data.TreeModel', {
-                        fileName: fileName,
-                        leaf: isLeaf,
-                        expanded: true,
-                        draggable: true,
-                    });
 
-                    if (!selectedNode[0].isLeaf()) {
-                        selectedNode[0].appendChild(newNode);
-                    } else {
-                        selectedNode[0].parentNode.appendChild(newNode);
+                let controller = new Explorer.view.main.ExplorerController();
+
+                if (controller.onSaveClick(fileName, selectedNode)) {
+
+
+
+                    var title = this.lookupViewModel().get('title');
+                    // console.log('Title:', title);
+                    let isLeaf = true;
+                    if (title === 'Folder') {
+                        // console.log('inside folder')
+                        isLeaf = false;
                     }
-                    this.up('window').destroy();
-                } else {
-                    // Handle case when no node is selected
-                    console.error('Error while adding the file');
+                    if (selectedNode && selectedNode.length == 1 && fileName) {
+                        let newNode = Ext.create('Ext.data.TreeModel', {
+                            fileName: fileName,
+                            leaf: isLeaf,
+                            expanded: true,
+                            draggable: true,
+                        });
+
+                        if (!selectedNode[0].isLeaf()) {
+                            selectedNode[0].appendChild(newNode);
+                        } else {
+                            selectedNode[0].parentNode.appendChild(newNode);
+                        }
+                        this.up('window').destroy();
+                    } else {
+                        // Handle case when no node is selected
+                        console.error('Error while adding the file');
+                    }
                 }
+                controller.destroy();
             }
+            // handler: function () {
+            //     var fileName = this.up('form').getForm().getFieldValues().fileName;
+            //     let selectedNode = Ext.getCmp('expView').getSelection();
+
+            //     var title = this.lookupViewModel().get('title');
+            //     // console.log('Title:', title);
+            //     let isLeaf = true;
+            //     if (title === 'Folder') {
+            //         // console.log('inside folder')
+            //         isLeaf = false;
+            //     }
+            //     if (selectedNode && selectedNode.length == 1 && fileName) {
+            //         let newNode = Ext.create('Ext.data.TreeModel', {
+            //             fileName: fileName,
+            //             leaf: isLeaf,
+            //             expanded: true,
+            //             draggable: true,
+            //         });
+
+            //         if (!selectedNode[0].isLeaf()) {
+            //             selectedNode[0].appendChild(newNode);
+            //         } else {
+            //             selectedNode[0].parentNode.appendChild(newNode);
+            //         }
+            //         this.up('window').destroy();
+            //     } else {
+            //         // Handle case when no node is selected
+            //         console.error('Error while adding the file');
+            //     }
+            // }
         }]
     }]
 });
